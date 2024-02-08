0f036001a7e9d588d9cabcf9a31d22a6a76c2c99
heoraengi <heo.raeng.i@gmail.com>
2024-02-08 10:30:17 +0900

    v0.1 combination

--- pythonProject/day01/day01.py
+++ pythonProject/day01/day01.py
@@ -1,1 +1,21 @@
-print("hello")
+def factorial(number) -> int:
+    if number <=1 :
+        return number
+    return number * factorial(number-1)
+
+def nCr(n,r) -> int:
+    """
+    조합 함수 생성
+    :param n:
+    :param r:
+    :return: 경우의 수
+    """
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return numerator / denominator
+
+if __name__ == "__main__":
+    n = int(input("Input n : "))
+    r = int(input("Input r : "))
+
+    print(f'{n}C{r} = {nCr(n,r)}')
