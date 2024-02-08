e3ec5cdd088faaf1469363adc21781586f4822bb
heoraengi <heo.raeng.i@gmail.com>
2024-02-08 15:43:35 +0900

    v0.3 polynomial

--- pythonProject/day01/polynomial.py
+++ pythonProject/day01/polynomial.py
@@ -1,34 +1,30 @@
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1
+def print_poly(t_x, f_x) -> str:
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
+        term = t_x[i]
         coefficient = f_x[i]
-        if coefficient >= 0 and i!=0 and coefficient != 0:
+        if coefficient >= 0 and i!=0 :
             poly_expression += "+"
-        if coefficient != 0 :
-            poly_expression += f"{coefficient}x^{term} "
-        term -= 1
+        poly_expression += f"{coefficient}x^{term} "
 
     return poly_expression
-
-
-def calculation_poly(x_value, f_x) -> int:
+def calculation_poly(x_value, t_x, f_x) -> int:
     return_value = 0
-    term = len(f_x) - 1
 
     for i in range(len(fx)):
+        term = t_x[i]
         coefficient = f_x[i]
         return_value += coefficient * pow(x_value, term)
         term -= 1
-
     return return_value
 
-fx = [2, 3, 4, 0, -9]
+tx = [10, 2, 0]
+fx = [7, 3, 4]
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    print(print_poly(fx))
-    print(calculation_poly(int(input("input x : ")), fx))
+    print(print_poly(tx,fx))
+    print(calculation_poly(int(input("input x : ")), tx, fx))
 
 
