18c4025b19afaefb9c70131ef2c52a6b89792439
heoraengi <heo.raeng.i@gmail.com>
2024-02-08 15:04:39 +0900

    v0.1 polynomial

---
+++ pythonProject/day01/polynomial.py
@@ -0,0 +1,34 @@
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression += "+"
+        poly_expression += f"{coefficient}x^{term} "
+        term -= 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term -= 1
+
+    return return_value
+
+fx = [2, 3, 4, 0, -9]
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("input x : ")), fx))
+
+
