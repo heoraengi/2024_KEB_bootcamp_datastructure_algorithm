6873fba401bdf22eec6358ed4f73f62790683e84
heoraengi <heo.raeng.i@gmail.com>
2024-02-14 14:38:50 +0900

    v0.3 day03 graph dfs

--- pythonProject/day03/day03.py
+++ pythonProject/day03/day03.py
@@ -14,12 +14,32 @@ graph2= [
     [0, 0, 1, 0, 0],
 ]
 
+graph2= [
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0],
+]
+
+
+graph3= [
+    [0, 1, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 1, 1],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0],
+]
+
+name_arr = ['mb','sr','hi','zz','sm','hs']
+
 def dfs(g, v, visited) :
     visited[v] = True
-    print(chr(ord('A')+v), end =' ')
+    print(name_arr[v], end =' ')
     for i in range(len(g)):
         if g[v][i] == True and not visited[i]:
             dfs(g, i, visited)
 
-visited = [False] * len(graph)
-dfs(graph2, 0, visited)
+visited = [False] * len(graph3)
+dfs(graph3, 0, visited)
