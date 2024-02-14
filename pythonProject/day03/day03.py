1fa08b44ce61fbd59700ebab966fb5899310cf9b
heoraengi <heo.raeng.i@gmail.com>
2024-02-14 16:15:14 +0900

    v0.1 day03 recursion

--- pythonProject/day03/day03.py
+++ pythonProject/day03/day03.py
@@ -1,45 +1,13 @@
-graph= [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 1, 0],
-    [0, 1, 0, 0, 1],
-    [1, 1, 0, 0, 1],
-    [0, 0, 1, 1, 0],
-]
+def decimal_to_octal(number: int) -> str:
+    '''
+    decimal number to octal number (by recursion)
+    :param number: integer (base dec)
+    :return: string (base octal)
+    '''
+    if number < 8 :
+        return str(number)
+    else :
+        return  decimal_to_octal(n//8) + str(n%8)
 
-graph2= [
-    [0, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-]
-
-graph2= [
-    [0, 0, 1, 1, 0],
-    [0, 0, 1, 0, 0],
-    [1, 1, 0, 1, 1],
-    [1, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-]
-
-
-graph3= [
-    [0, 1, 1, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0],
-    [1, 0, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1, 1],
-    [0, 0, 0, 1, 0, 1],
-    [0, 0, 0, 1, 1, 0],
-]
-
-name_arr = ['mb','sr','hi','zz','sm','hs']
-
-def dfs(g, v, visited) :
-    visited[v] = True
-    print(name_arr[v], end =' ')
-    for i in range(len(g)):
-        if g[v][i] == True and not visited[i]:
-            dfs(g, i, visited)
-
-visited = [False] * len(graph3)
-dfs(graph3, 0, visited)
+n = int(input("Input decimal number : "))
+print(decimal_to_octal(n))
