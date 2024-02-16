08a241d1645108261f63bb9e4bf62b7e4e75f017
heoraengi <heo.raeng.i@gmail.com>
2024-02-16 11:37:28 +0900

    v0.2 bfs

--- pythonProject/day05/day05.py
+++ pythonProject/day05/day05.py
@@ -1,31 +1,36 @@
-def fibo_memo(n):
-    global memo, cnt_memo
+from collections import deque
+def dfs(g, i, visited):
+    visited[i] = 1
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-    if memo[n] is not None:
-        return memo[n]
-    if n < 2 :
-        result = n
-    else :
-        cnt_memo += 1
-        result = fibo_memo(n-1) + fibo_memo(n-2)
-        memo[n] = result
-    return result
+def bfs(g, i, visited):
+    queue = deque([i])
+    visited[i] = 1
+    while queue:
+        i = queue.popleft()
+        print(chr(ord('A') + i), end=' ')
+        for j in range(len(g)):
+            if g[i][j] == 1 and not visited[j]:
+                queue.append(j)
+                visited[j] = 1
 
-def fibo(n) :
-    global cnt
-    cnt +=1
-    if n < 2:
-        return n
-    else:
-        return fibo(n-1) + fibo(n-2)
 
-cnt_memo = 0
-cnt = 0
-memo = [None] * 100
-memo[0] = 0
-memo[1] = 1
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1 ,0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0 ,0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
-print(fibo(30))
-print(fibo_memo(30))
-print(cnt)
-print(cnt_memo)
+visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 0, visited_dfs)
+print()
+bfs(graph, 0, visited_bfs)
